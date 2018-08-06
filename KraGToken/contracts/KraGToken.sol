2a1cf0497c45cf3d8387b76e2e3a0aeb1bcc9895
BKVerkerk <875393@students.wits.ac.za>
2018-08-06 19:40:55 +0200

    Added comments to KragToken contract

--- KraGToken/contracts/KraGToken.sol
+++ KraGToken/contracts/KraGToken.sol
@@ -6,10 +6,10 @@ import "./MeterManagement.sol";
 contract KraGToken is MeterManagement {
     string public name = "KraG Token: 1 token = 1kWh";
     string public symbol = "KraG";
-    uint8 public decimals = 6;
+    uint8 public decimals = 6; // Defines number of decimal places for Krag Token 
     
     
     constructor() public {
-        totalSupply_ = 0;
+        totalSupply_ = 0; // Initializes total token supply to 0 upon intial deployment
     }
 }
