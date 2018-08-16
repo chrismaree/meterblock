ddb8dd0940c7033f76be7a4cfc75fc55ad5826a5
SoIidarity <christopher.maree@gmail.com>
2018-08-16 13:31:41 +0200

    Added basic front end and graph structure to store measurment records

--- BackendServer/gunAPI/routes/routes.js
+++ BackendServer/gunAPI/routes/routes.js
@@ -2,13 +2,31 @@ const Gun = require('gun')
 var gun = Gun("http://localhost:8080/gun");
 
 
+function checkContentType(req, res, next) {
+    if (!req.is('application/json')) {
+        return res.status(400).send({
+            ok: false,
+            error: "Bad request"
+        });
+    }
+
+    next();
+}
+
 var appRouter = function (app) {
-    app.get("/", function (req, res) {
-        res.status(200).send("Welcome to our restful API");
-    });
+
 app.post("/addEntry", function(req,res){
     console.log(req.body);
-    gun.get(req.body.key).put(req.body.value);    
+    var newRecord = gun.get(req.body.time).put(req.body.value)
+    var meterRecords = gun.get(req.body.key)
+    meterRecords.set(newRecord)
+    
+    
+    meterRecords.map().once(function (person) {
+        console.log("The record is", person);
+    });
+    
+        
     res.status(200).send();
 });
 
