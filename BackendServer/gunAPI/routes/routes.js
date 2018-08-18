fe76d652b8964f11b00895ff1717cfa9cd6d860b
SoIidarity <christopher.maree@gmail.com>
2018-08-18 09:08:51 +0200

    Updated gunAPI

--- BackendServer/gunAPI/routes/routes.js
+++ BackendServer/gunAPI/routes/routes.js
@@ -1,7 +1,6 @@
 const Gun = require('gun')
 var gun = Gun("http://localhost:8080/gun");
 
-
 function checkContentType(req, res, next) {
     if (!req.is('application/json')) {
         return res.status(400).send({
@@ -9,27 +8,31 @@ function checkContentType(req, res, next) {
             error: "Bad request"
         });
     }
-
     next();
 }
-
 var appRouter = function (app) {
-
-app.post("/addEntry", function(req,res){
-    console.log(req.body);
-    var newRecord = gun.get(req.body.time).put(req.body.value)
-    var meterRecords = gun.get(req.body.key)
-    meterRecords.set(newRecord)
     
+    // Expose an post API enpoint to enable the caller to add records to the 
+    // GUNDB graph database. The expected data structure is:
+    // {
+    //  "key": "ETH PUB KEY",
+    //  "value": {
+    //     "power": "POWER CONSUMPTION AT THAT POINT",
+    //     "tokens": 1234567890
+    // },
+    //  "time": 1234567890 UNIX TIME STAMP
+    // }
     
-    meterRecords.map().once(function (person) {
-        console.log("The record is", person);
+    app.post("/addEntry", function(req,res){
+        console.log(req.body);
+        var newRecord = gun.get(req.body.time).put(req.body.value)
+        var meterRecords = gun.get(req.body.key)
+        meterRecords.set(newRecord)
+        meterRecords.map().once(function (person) {
+            console.log("The record is", person);
+        });
+        res.status(200).send();
     });
-    
-        
-    res.status(200).send();
-});
-
 }
 
 module.exports = appRouter;
