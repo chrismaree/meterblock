c179d85bb1ea5ece184f37a9a4e84fa9a66e3739
SoIidarity <christopher.maree@gmail.com>
2018-08-23 11:07:11 +0200

    Basic network graph implementation

--- frontend/src/components/ViewAllMeters.vue
+++ frontend/src/components/ViewAllMeters.vue
@@ -21,6 +21,9 @@
         </el-tag>
         <br>
         {{meterData}}
+        <br>
+        <br>
+        {{datacollection}}
         
         <br>
         <el-radio-group v-model="chartMode">
@@ -30,6 +33,11 @@
         <el-radio-button label="Week"></el-radio-button>
         <el-radio-button label="Month"></el-radio-button>
     </el-radio-group>
+<br>
+<el-card class="box-card" shadow="always" >
+    <Bar :options="chartOptions" :chart-data="datacollection" style="hight:800px !important"></Bar>
+  </el-card>
+
   </div>
 </template>
 
@@ -41,6 +49,9 @@ import { loadKragToken, getAllMeters } from "../../utils/KraGTokenInterface";
 
 export default {
   name: "ViewAllMeters",
+  components: {
+    Bar
+  },
   data() {
     return {
       allMeterAddresses: [],
@@ -49,7 +60,35 @@ export default {
       chartMode: "Hour",
       selectedMeters: [],
       tagTypes: ["", "success", "info", "warning", "danger"],
-      meterData: {}
+      backgroundColors: ["blue", "green", "red"],
+      meterData: {},
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        segmentShowStroke: true,
+        scales: {
+          yAxes: [
+            {
+              stacked: true,
+              display: true,
+              labelString: "Power(w)",
+              ticks: {
+                autoSkip: true,
+                suggestedMin: 0 // minimum will be 0, unless there is a lower value.
+              }
+            }
+          ],
+          xAxes: [
+            {
+              stacked: true,
+              ticks: {
+                autoSkip: true
+              },
+              labelString: "time"
+            }
+          ]
+        }
+      }
     };
   },
   async created() {
@@ -82,9 +121,9 @@ export default {
       this.$data.meterData[_meterAddress].tokens.push(tokens);
     },
     handleClose(tag) {
+      delete this.meterData[tag.name];
       this.tags.splice(this.tags.indexOf(tag), 1);
       this.selectedMeters.splice(this.tags.indexOf(tag), 1);
-      this.inputMeter = "";
     },
     handleSelect() {
       if (
@@ -100,90 +139,142 @@ export default {
         });
         this.addMeter(this.inputMeter);
       }
+      this.inputMeter = "";
+    },
+    timeConverter(UNIX_timestamp, dataType) {
+      var a = new Date(UNIX_timestamp);
+      var months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes() < 10 ? "0" + a.getMinutes() : a.getMinutes();
+      var sec = a.getSeconds() < 10 ? "0" + a.getSeconds() : a.getSeconds();
+      if (dataType == "hour") {
+        return hour + ":" + min + ":" + sec;
+      }
+      if (dataType == "day") {
+        return month + "/" + date + " " + hour + ":" + min;
+      }
+      if (dataType == "month") {
+        return date + "/" + month + " " + hour + ":" + min;
+      }
+      if (dataType == "year") {
+        return year + "/" + month + "/" + date;
+      }
     }
   },
 
   computed: {
     datacollection() {
       try {
-        let number;
+        let number = 30;
         let timeStamps = [];
         if (this.chartMode == "Minute") {
-          if (this.$data.meterData.lables[0].length > 30) {
+          if (
+            this.$data.meterData[Object.keys(this.$data.meterData)[0]].lables[0]
+              .length > 30
+          ) {
             number = 30;
-          } else number = number = this.$data.meterData.lables[0].length;
+          } else
+            number = this.$data.meterData[
+              Object.keys(this.$data.meterData)[0]
+            ].lables[0].length;
 
-          for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
-            Math.max(this.$data.meterData.lables[0].length - number, 0)
+          for (let unixTimeStamp of this.$data.meterData[
+            Object.keys(this.$data.meterData)[0]
+          ].lables[0].slice(
+            Math.max(
+              this.$data.meterData[Object.keys(this.$data.meterData)[0]]
+                .lables[0].length - number,
+              0
+            )
           )) {
             var date = new Date(unixTimeStamp * 1000);
             timeStamps.push(this.timeConverter(date, "hour"));
           }
         }
+        // console.log(timeStamps);
+        // if (this.chartMode == "Hour") {
+        //   if (this.$data.meterData.lables[0].length > 1800) {
+        //     number = 1800;
+        //   } else number = number = this.$data.meterData.lables[0].length;
+        //   for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
+        //     Math.max(this.$data.meterData.lables[0].length - number, 0)
+        //   )) {
+        //     var date = new Date(unixTimeStamp * 1000);
+        //     timeStamps.push(this.timeConverter(date, "hour"));
+        //   }
+        // }
 
-        if (this.chartMode == "Hour") {
-          if (this.$data.meterData.lables[0].length > 1800) {
-            number = 1800;
-          } else number = number = this.$data.meterData.lables[0].length;
-          for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
-            Math.max(this.$data.meterData.lables[0].length - number, 0)
-          )) {
-            var date = new Date(unixTimeStamp * 1000);
-            timeStamps.push(this.timeConverter(date, "hour"));
-          }
+        // if (this.chartMode == "Day") {
+        //   if (this.$data.meterData.lables[0].length > 43200) {
+        //     number = 43200;
+        //   } else number = number = this.$data.meterData.lables[0].length;
+        //   for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
+        //     Math.max(this.$data.meterData.lables[0].length - number, 0)
+        //   )) {
+        //     var date = new Date(unixTimeStamp * 1000);
+        //     timeStamps.push(this.timeConverter(date, "day"));
+        //   }
+        // }
+
+        // if (this.chartMode == "Week") {
+        //   if (this.$data.meterData.lables[0].length > 302400) {
+        //     number = 302400;
+        //   } else number = number = this.$data.meterData.lables[0].length;
+        //   for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
+        //     Math.max(this.$data.meterData.lables[0].length - number, 0)
+        //   )) {
+        //     var date = new Date(unixTimeStamp * 1000);
+        //     timeStamps.push(this.timeConverter(date, "month"));
+        //   }
+        // }
+
+        // if (this.chartMode == "Month") {
+        //   if (this.$data.meterData.lables[0].length > 1296000) {
+        //     number = 1296000;
+        //   } else number = number = this.$data.meterData.lables[0].length;
+        //   for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
+        //     Math.max(this.$data.meterData.lables[0].length - number, 0)
+        //   )) {
+        //     var date = new Date(unixTimeStamp * 1000);
+        //     timeStamps.push(this.timeConverter(date, "month"));
+        //   }
+        // }
+
+        console.log("KEY POINT");
+        let keys = Object.keys(this.$data.meterData);
+        console.log(keys);
+        let dataValues = { labels: timeStamps, datasets: [] };
+        for (let i = 0; i < keys.length; i++) {
+          console.log(this.meterData[keys[i]]);
+          dataValues.datasets.push({
+            label: keys[i] + " Power Consumption",
+            data: this.meterData[keys[i]].values[0].slice(
+              Math.max(this.meterData[keys[i]].values[0].length - number, 0)
+            ),
+            backgroundColor: this.backgroundColors[i]
+          });
         }
+        console.log("DATA")
+        console.log(dataValues)
+        return dataValues;
 
-        if (this.chartMode == "Day") {
-          if (this.$data.meterData.lables[0].length > 43200) {
-            number = 43200;
-          } else number = number = this.$data.meterData.lables[0].length;
-          for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
-            Math.max(this.$data.meterData.lables[0].length - number, 0)
-          )) {
-            var date = new Date(unixTimeStamp * 1000);
-            timeStamps.push(this.timeConverter(date, "day"));
-          }
-        }
-
-        if (this.chartMode == "Week") {
-          if (this.$data.meterData.lables[0].length > 302400) {
-            number = 302400;
-          } else number = number = this.$data.meterData.lables[0].length;
-          for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
-            Math.max(this.$data.meterData.lables[0].length - number, 0)
-          )) {
-            var date = new Date(unixTimeStamp * 1000);
-            timeStamps.push(this.timeConverter(date, "month"));
-          }
-        }
-
-        if (this.chartMode == "Month") {
-          if (this.$data.meterData.lables[0].length > 1296000) {
-            number = 1296000;
-          } else number = number = this.$data.meterData.lables[0].length;
-          for (let unixTimeStamp of this.$data.meterData.lables[0].slice(
-            Math.max(this.$data.meterData.lables[0].length - number, 0)
-          )) {
-            var date = new Date(unixTimeStamp * 1000);
-            timeStamps.push(this.timeConverter(date, "month"));
-          }
-        }
-
-        return {
-          labels: timeStamps,
-          datasets: [
-            {
-              label: this.selectedMeter + " Power Consumption",
-              data: this.$data.meterData.values[0].slice(
-                Math.max(this.$data.meterData.lables[0].length - number, 0)
-              ),
-              backgroundColor: Array.apply(null, Array(number)).map(
-                String.prototype.valueOf,
-                "#7ea9d6"
-              )
-            }
-          ]
-        };
+        return "a";
       } catch (e) {
         console.log(e);
         return null;
